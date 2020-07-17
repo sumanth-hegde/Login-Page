97c76d22ab6e812e9eac0d9345b4bc1fca1b3fe5
Sumanth <ssh@gmail.com>
2020-07-17 17:39:08 +0530

    Added home page with background image.

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+	<link rel="stylesheet" type="text/css" href="homestyle.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body class="body">
+	<div class="align">
+		<h1 class="text-danger"><b>Welcome</b></h1>
+		<div>
+			<form action="index.php">
+				<p class="text-success"><b>To Logout Click here</b></p>
+				<input type="submit" class="btn btn-info" value="Logout" name="">
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>

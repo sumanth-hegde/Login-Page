159c5575b1968609647fc9f10ea2e14808570440
Sumanth <ssh@gmail.com>
2020-07-17 17:42:14 +0530

    Added index page with css file.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+	<link rel="stylesheet" type="text/css" href="indexstyle.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body class="body">
+<h1 class="text-info"><b>Welcome</b></h1>
+
+<div>
+	<form action="login.php" >
+		<p class="text-warning"><b>To Login in click here</b></p>
+		<input type="submit" class="btn btn-primary" value="Login" name="login"><br>
+	</form>
+</div>
+
+
+<div>
+	<form action="register.php">
+		<p class="text-warning"><b>To register in click here</b></p>	
+<input type="submit" class="btn btn-primary " value="Register" name="register"><br>
+	</form>
+</div>
+
+
+</body>
+</html>
+
